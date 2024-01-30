5ad5e6085ec4827b91c9817e98a57be76ea066f7
Juan-Ceballos <juanceballos@pursuit.org>
2024-01-30 00:57:19 -0500

    first commit

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-AppUITests/Pursuit_CodeTrack_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pursuit_CodeTrack_AppUITestsLaunchTests.swift
+//  Pursuit-CodeTrack-AppUITests
+//
+//  Created by Juan ceballos on 1/30/24.
+//
+
+import XCTest
+
+final class Pursuit_CodeTrack_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Pursuit-CodeTrack-App
