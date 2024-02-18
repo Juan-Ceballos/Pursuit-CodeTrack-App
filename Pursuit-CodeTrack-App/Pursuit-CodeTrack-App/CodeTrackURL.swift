fb9bbae8a7a2e93cecb5a9d008e3936239ea383a
Juan-Ceballos <juanceballos@pursuit.org>
2024-02-17 20:24:44 -0500

    added base urls for api

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/CodeTrackURL.swift
@@ -0,0 +1,26 @@
+//
+//  CodeTrackURL.swift
+//  Pursuit-CodeTrack-App
+//
+//  Created by Juan ceballos on 2/17/24.
+//
+
+import Foundation
+
+class CodeTrackURL {
+    static let orgPursuit = "https://codewars-tracker-be.herokuapp.com" +
+                            "/organizations/organization_profile/pursuit"
+    
+    static let scoreboard = "https://codewars-tracker-be.herokuapp.com/" +
+                            "scoreboard/pursuit"
+    
+    static let standings = "https://codewars-tracker-be.herokuapp.com/" +
+                            "dailystandings/pursuit"
+    
+    static let users = "https://codewars-tracker-be.herokuapp.com/" +
+                        "users?organization=pursuit"
+    
+    static let role = "https://codewars-tracker-be.herokuapp.com/" +
+                    "users?role=staff&organization=pursuit"
+    
+}
