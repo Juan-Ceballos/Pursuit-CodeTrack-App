04785ee05aee64490c747ec95102fa62cff41953
Juan-Ceballos <juanceballos@pursuit.org>
2024-03-03 21:45:17 -0500

    added tab bar controller and first vc tab

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabBarController.swift
+//  Pursuit-CodeTrack-App
+//
+//  Created by Juan ceballos on 3/3/24.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    public lazy var userScoresViewController: UIViewController =  {
+        let vc = ViewController()
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), tag: 0)
+        return vc
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [UINavigationController(rootViewController: userScoresViewController)]
+    }
+}
