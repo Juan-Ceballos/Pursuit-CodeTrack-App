171e2cb5990e04065014352fd69a3e2e4761001b
Juan-Ceballos <juanceballos@pursuit.org>
2024-03-12 01:53:15 -0400

    view for main vc

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/Views/UserScoresView.swift
@@ -0,0 +1,23 @@
+//
+//  UserScoresView.swift
+//  Pursuit-CodeTrack-App
+//
+//  Created by Juan ceballos on 3/12/24.
+//
+
+import UIKit
+
+
+class UserScoresView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func commonInit() {
+        
+    }
+}
