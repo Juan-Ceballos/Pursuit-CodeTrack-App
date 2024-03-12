0615d2cd7b54dd9f0fa0159306d57e86dd004fa2
Juan-Ceballos <juanceballos@pursuit.org>
2024-03-12 01:44:26 -0400

    added styling for consistent and facilitate assigning color to ui components

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/SupportingFiles/AppStyling.swift
@@ -0,0 +1,21 @@
+//
+//  AppStyling.swift
+//  Pursuit-CodeTrack-App
+//
+//  Created by Juan ceballos on 3/12/24.
+//
+
+import UIKit
+
+struct AppColors {
+    static let primaryColor = UIColor.systemBackground
+    static let secondaryColor = UIColor.systemBlue
+}
+
+struct AppFonts {
+    static let labelFont = UIFont.systemFont(ofSize: 18)
+}
+
+struct AppSizes {
+    static let noPadding: CGFloat = 0
+}
