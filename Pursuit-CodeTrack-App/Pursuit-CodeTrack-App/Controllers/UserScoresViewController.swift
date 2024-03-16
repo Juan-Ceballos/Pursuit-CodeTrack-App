5c1354a154f0509c336bcf909bcbb70741c166f4
Juan-Ceballos <juanceballos@pursuit.org>
2024-03-16 19:36:29 -0400

    fetch and set scoreboard data for scoreboard ui

--- Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/Controllers/UserScoresViewController.swift
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/Controllers/UserScoresViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 class UserScoresViewController: UIViewController {
     
     let userScoreView = UserScoresView()
+    var scoreboard: ScoreboardModel?
+    
+    init(scoreboard: ScoreboardModel? = nil) {
+        self.scoreboard = scoreboard
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = userScoreView
@@ -17,11 +27,24 @@ class UserScoresViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setScoreboardModel()
     }
     
-    func fetchScoreboardData() {
-        
+    func fetchScoreboardData() async -> ScoreboardModel? {
+        do {
+            let scoreboard: ScoreboardModel = try await CodeTrackAPI.shared.fetchCodeTrack(CodeTrackURL.scoreboard)
+            return scoreboard
+        } catch {
+            print("\(error)")
+        }
+        return nil
     }
+    
+    func setScoreboardModel() {
+        Task {
+            await scoreboard = fetchScoreboardData()
+        }
+    }
+    
 }
 
