bf722b38c1fbb89a401097db5c0cfaf7e110c441
Juan-Ceballos <juanceballos@pursuit.org>
2024-03-29 22:46:18 -0400

    started cell for collection view

---
+++ Pursuit-CodeTrack-App/Pursuit-CodeTrack-App/Views/UserScoreCardCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserScoreCardCell.swift
+//  Pursuit-CodeTrack-App
+//
+//  Created by Juan ceballos on 3/29/24.
+//
+
+import UIKit
+
+class UserScoreCardCell: UICollectionViewCell {
+    
+}
